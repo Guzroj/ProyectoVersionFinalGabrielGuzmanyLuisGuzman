06e5b56c27971d7793887875c966d2f9ee40657f
Gabriel <gabguzman@estudiantec.cr>
2022-09-28 16:23:58 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module p.proyectoversionfinalgabrielguzmanyluisguzman {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens p.proyectoversionfinalgabrielguzmanyluisguzman to javafx.fxml;
+    exports p.proyectoversionfinalgabrielguzmanyluisguzman;
+}
